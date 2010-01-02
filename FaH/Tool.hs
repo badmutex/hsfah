c9c571b24110fc3e702639c526d698968faa9ace
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-01 20:53:16 -0500

    all a tool should know is the run, clone it is on, and the path to the work area and root project path

--- FaH/Tool.hs
+++ FaH/Tool.hs
@@ -11,28 +11,19 @@ import FaH.Types
 import Control.Applicative ((<$>))
 import Control.Monad (mapM_, sequence_)
 
-applyTool :: Tool -> TrajectoryLocation -> Action
+applyTool :: Tool -> ToolInfo -> Action
 applyTool t = t
 
-applyAllTools :: [Tool] -> TrajectoryLocation -> [Action]
+applyAllTools :: [Tool] -> ToolInfo -> [Action]
 applyAllTools ts ps = map (flip applyTool ps) ts
 
-doAllTools :: [Tool] -> TrajectoryLocation -> IO [Status]
+doAllTools :: [Tool] -> ToolInfo -> IO [Status]
 doAllTools ts = sequence . applyAllTools ts
 
-doAllTrajs :: [Tool] -> [TrajectoryLocation] -> IO [Status]
+doAllTrajs :: [Tool] -> [ToolInfo] -> IO [Status]
 doAllTrajs ts trajs = concat <$> mapM (doAllTools ts) trajs
 
-instance Apply Tool TrajectoryLocation Action            where apply = applyTool
-instance Apply [Tool] TrajectoryLocation [Action]        where apply = applyAllTools
-instance Apply [Tool] TrajectoryLocation (IO [Status])   where apply = doAllTools
-instance Apply [Tool] [TrajectoryLocation] (IO [Status]) where apply = doAllTrajs
-
-
-
-mkTrajectory :: Run -> Clone -> FilePath -> Trajectory
-mkTrajectory r c p = Trajectory { run = r, clone = c, location = p }
-
-mkProject :: [Trajectory] -> Project
-mkProject = Project
-
+instance Apply Tool ToolInfo Action            where apply = applyTool
+instance Apply [Tool] ToolInfo [Action]        where apply = applyAllTools
+instance Apply [Tool] ToolInfo (IO [Status])   where apply = doAllTools
+instance Apply [Tool] [ToolInfo] (IO [Status]) where apply = doAllTrajs
--- FaH/Types.hs
+++ FaH/Types.hs
@@ -13,27 +13,33 @@ import qualified  Database.HDBC as DB
 
 data PRun
 data PClone
+data PProjArea
+data PWorkArea
 
+-- if the types need to be changed in the database, 
+-- this is the place to do so
 _db_structure_id_type = "integer"
-_db_run_type = "integer"
-_db_clone_type = "integer"
-_db_frame_type = "integer"
-_db_rep_type = "integer"
+_db_run_type          = "integer"
+_db_clone_type        = "integer"
+_db_frame_type        = "integer"
+_db_rep_type          = "integer"
 
-type Run = Tagged PRun Integer
-type Clone = Tagged PClone Integer
+-- I shouldn't be able to treat runs and clones as the same.
+-- Same goes for the workarea/project paths
+type Run      = Tagged PRun Integer
+type Clone    = Tagged PClone Integer
+type WorkArea = Tagged PWorkArea FilePath
+type ProjArea = Tagged PProjArea FilePath
 
-data TrajectoryLocation = TrajLoc Run Clone FilePath
 
-data Project = Project {
-      trajectories :: [Trajectory]
-    } deriving (Eq, Show)
+-- The info that a tool has access to
+data ToolInfo = ToolInfo {
+      run         :: Run
+    , clone       :: Clone
+    , workarea    :: WorkArea
+    , projectArea :: ProjArea
+    }
 
-data Trajectory = Trajectory {
-      run            :: Run
-    , clone          :: Clone
-    , _location :: FilePath
-    } deriving (Eq, Show)
 
 
 type Stat a = Either String a
@@ -41,7 +47,7 @@ type Status = Stat ()
 
 type Action = IO Status
 
-type Tool = TrajectoryLocation -> Action
+type Tool = ToolInfo -> Action
 
 class Apply a b c where apply :: a -> b -> c
 
