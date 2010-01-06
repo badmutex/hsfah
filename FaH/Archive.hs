3975e7844e66ec8b7a78ee4e1b6fa6f9319ea606
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-06 17:41:49 -0500

    prettify code, stronger type descriptions for Archive module

--- FaH/Archive.hs
+++ FaH/Archive.hs
@@ -3,19 +3,20 @@ module FaH.Archive where
 
 import FaH.Types
 
-import Control.Applicative ((<$>))
-import Codec.Compression.BZip
-import Text.Printf (printf)
+import Control.Applicative    ((<$>))
+import Codec.Compression.BZip (decompress)
+import Text.Printf            (printf)
 
-import qualified Codec.Archive.Tar as Tar
+import qualified Codec.Archive.Tar       as Tar
 import qualified Codec.Archive.Tar.Check as Tar
-import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Lazy    as BS
 
 
+type Tarball   = FilePath
+type TargetDir = FilePath
 
-unpack_tarbz2 :: FilePath -- ^ the tarball
-              -> FilePath -- ^ target directory
-              -> IO ()
+
+unpack_tarbz2 :: Tarball -> TargetDir -> IO ()
 unpack_tarbz2 tarball targetdir = do
   entries <- Tar.read . decompress <$> BS.readFile tarball
   Tar.unpack targetdir entries
