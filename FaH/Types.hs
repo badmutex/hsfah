b07a3f3d7a46e4f98dc40f11b30655956fde1114
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-06 18:01:37 -0500

    use type classes to get at the run/clone info

--- FaH/Types.hs
+++ FaH/Types.hs
@@ -5,6 +5,9 @@
 module FaH.Types ( Run, Clone
                  , ProjArea, WorkArea
 
+                 , RunVal (..), CloneVal (..)
+                 , WorkAreaVal (..), ProjAreaVal (..)
+
                  , Message (..)
                  , Log (..)
                  , Logger
@@ -41,15 +44,32 @@ data Message a = Stop | Msg a
 newtype Log    = Log String
 type Logger    = Log -> IO ()
 
+class RunVal a where run :: a -> Run
+class CloneVal a where clone :: a -> Clone
+class WorkAreaVal a where workArea :: a -> WorkArea
+class ProjAreaVal a where projArea :: a -> ProjArea
 
 data ToolInfo  = ToolInfo {
-      run      :: Run
-    , clone    :: Clone
-    , workArea :: WorkArea
+      ti_run      :: Run
+    , ti_clone    :: Clone
+    , ti_workArea :: WorkArea
     , logger   :: Logger
     }
 
-data TrajInfo = TrajInfo Run [Clone] WorkArea
+instance RunVal ToolInfo where run = ti_run
+instance CloneVal ToolInfo where clone = ti_clone
+instance WorkAreaVal ToolInfo where workArea = ti_workArea
+
+data TrajInfo = TrajInfo {
+      traj_run :: Run
+    , clones :: [Clone]
+    , traj_workArea :: WorkArea
+    , traj_projArea :: ProjArea
+    }
+
+instance RunVal TrajInfo where run = traj_run
+instance WorkAreaVal TrajInfo where workArea = traj_workArea
+instance ProjAreaVal TrajInfo where projArea = traj_projArea
 
 
 type Tool a = ErrorT String (StateT ToolInfo IO) a
