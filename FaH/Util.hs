683cb9a3b62f1e8c5d22cb6073028f7454d2b1fd
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-06 18:02:25 -0500

    figure out the traj path from the proj area, run, and clone

--- FaH/Util.hs
+++ FaH/Util.hs
@@ -7,6 +7,10 @@ import FaH.Types
 import Control.Concurrent
 import Control.Monad
 
+import Data.Tagged
+
+import System.FilePath ((</>))
+
 
 
 logger :: Chan (Message Log) -> IO ()
@@ -15,3 +19,10 @@ logger chan = forever $ do
                 case msg of
                   Stop        -> myThreadId >>= killThread
                   Msg (Log l) -> putStrLn l
+
+trajPath :: ProjArea -> Run -> Clone -> FilePath
+trajPath (Tagged wa)
+         (Tagged r)
+         (Tagged c) = wa </> "RUN" ++ show r </> "CLONE" ++ show c
+
+
