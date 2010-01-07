e25166c6fd6ecdaa9929fd612ec43d41f67d3a8e
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-07 18:03:20 -0500

    save

--- FaH/Tool/Protomol.hs
+++ FaH/Tool/Protomol.hs
@@ -13,11 +13,19 @@ import Control.Monad.List
 import Data.Tagged
 
 import System.FilePath
+import System.FilePath.Glob
 import System.Directory
 
+import Text.Printf
+
+_name = "FaH.Tool.Protomol"
+_results_glob = "results-???.tar.bz2"
+
+addLog' = addLog . printf "[%s] %s" _name
 
 
-handle_tarball :: WorkArea -> Tarball -> Tool a -> ErrorT String IO a
+handle_tarball :: MonadIO m =>
+                  WorkArea -> Tarball -> Tool a -> m (Either String a, [String])
 handle_tarball wa tball tool = do
   let name = takeFileName . dropExtension . dropExtension
       target = unTagged wa </> name tball
@@ -25,21 +33,21 @@ handle_tarball wa tball tool = do
   liftIO $ createDirectory target
   liftIO $ extract_tarbz2 tball target
 
-  lift $ runStateT tool undefined
-
-  -- liftIO $ removeDirectoryRecursive target
-
-  return undefined
+  liftIO $ runTool tool undefined
 
 
+tarballs :: TrajArea -> IO [Tarball]
+tarballs tra = globDir1 (compile _results_glob) (unTagged tra)
 
 
-
-
-protomol :: TrajTool a
-protomol trajinfo tool = do
+protomol :: Tool a -> FaH b
+protomol tool = do
+  addLog' "starting"
   
+  trinfo <- get
+  -- tarballs <- liftIO $ tarballs (
   
+  liftIO $ handle_tarball undefined undefined tool
   
 
   return undefined
