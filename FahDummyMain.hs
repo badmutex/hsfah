5aa2ce5db9517af4897b1f4bd460378d5f91e28d
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-09 21:02:28 -0500

    ignoreing dist folding and created dummy file that imports everthying

---
+++ FahDummyMain.hs
@@ -0,0 +1,11 @@
+
+
+module FahDummyMain where
+
+import FaH.Archive
+import FaH.Exceptions
+import FaH.Logging
+import FaH.Tool.Protomol
+import FaH.Tool.Protomol.VMD.RMSD
+import FaH.Types
+import FaH.Util
