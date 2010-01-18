43f4d41139cc42bb9a66cae041a0e8f7e8ce7199
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-17 19:29:22 -0500

    testing tool to calculate the number of frames for each generation

--- test/Project.hs
+++ test/Project.hs
@@ -6,6 +6,8 @@ import FaH.Logging
 import FaH.Util
 import FaH.Project
 import FaH.Tool.Protomol
+import FaH.Tool.Protomol.Generation
+import FaH.Tool.Protomol.CountFrames
 import FaH.Tool.Protomol.VMD.RMSD
 
 import Control.Applicative ((<$>))
@@ -13,6 +15,7 @@ import Data.Tagged
 import Data.Maybe
 import System.Directory
 import Data.List
+import Text.Printf
 
 
 _fah = "/home/badi/Research/fah/afs-crc-fah/fahnd01/data01/data/PROJ10001"
@@ -46,6 +49,12 @@ theTool = let ti = ToolInfo r c wa undefined
                 safeLiftIO $ appendFile "/tmp/hsfah.results" $ res ++ "\n"
                 return ()
 
+theTool2 :: Tool ()
+theTool2 = let tr = undefined :: ToolReader
+           in do
+             gen <- generation
+             frames <- frames (CatDCD "catdcd") (DCDFile "ww.dcd")
+             addLog $ printf "+=+=+=+=+=+= Gen %d Frames %s" gen (show frames)
 
 
-test = doProject proj theTool
+test = doProject proj (protomol theTool2)
